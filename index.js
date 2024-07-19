42f35f23a5b43ecb0d8642622021002ff95eb603
Tony Lam <tony.lam@iki.fi>
2024-07-19 14:25:31 +0200

    3.17*: puhelinluettelo ja tietokanta, step5

--- index.js
+++ index.js
@@ -36,29 +36,6 @@ app.use(
     )
 );
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendiekc",
-        number: "39-24-6423122",
-    },
-];
-
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>");
 });
@@ -124,13 +101,6 @@ app.post("/api/persons", (request, response, next) => {
         });
     }
 
-    // IGNORE DUPLICATE CHECKING FOR NOW
-    // if (persons.some((person) => person.name === body.name)) {
-    //     return response.status(400).json({
-    //         error: "name must be unique",
-    //     });
-    // }
-
     const person = new People({
         name: body.name,
         number: body.number,
@@ -140,6 +110,22 @@ app.post("/api/persons", (request, response, next) => {
         .save()
         .then((savedPerson) => {
             response.json(savedPerson);
+        })
+        .catch((error) => next(error));
+});
+
+// update person
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    People.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
         })
         .catch((error) => next(error));
 });
