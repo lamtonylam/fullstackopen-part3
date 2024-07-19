ce3fe8aab1e5ee472cb04e9eb6a8bfdc16857ad4
Tony Lam <tony.lam@iki.fi>
2024-07-19 17:10:47 +0200

    3.22: lint-konfiguraatio

--- models/people.js
+++ models/people.js
@@ -1,43 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log("connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message);
-    });
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 // mongodb person schema
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{7,8}/.test(v)
+      },
     },
-    number: {
-        type: String,
-        minlength: 8,
-        validate: {
-            validator: function (v) {
-                return /\d{2,3}-\d{7,8}/.test(v);
-            },
-        },
-    },
-});
+  },
+})
 
 // cleaning up the schema to correct mongodb id
-personSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
-});
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
